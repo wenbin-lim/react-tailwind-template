23552bb869ff3bb728f0756d25dc20ee76b842e2
wenbin <wenbinlim93@gmail.com>
2023-08-14 14:22:14 +0800

    add new vite plugin to allow dot in path url

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { reactClickToComponent } from "vite-plugin-react-click-to-component";
+import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     // vite config
-    plugins: [react(), svgr(), reactClickToComponent()],
+    plugins: [react(), svgr(), reactClickToComponent(), pluginRewriteAll()],
     server: {
       port: parseInt(env.VITE_SERVER_PORT),
       host: true,
