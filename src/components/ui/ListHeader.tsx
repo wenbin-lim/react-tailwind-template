a766f2fa4c4279e9bc9f572fd99e21e62985cd22
wenbin <wenbinlim93@gmail.com>
2023-08-16 16:01:17 +0800

    update ListHeader css

--- src/components/ui/ListHeader.tsx
+++ src/components/ui/ListHeader.tsx
@@ -11,11 +11,13 @@ const ListHeader = ({ title, buttons }: ListHeaderProps) => {
   return (
     <div className="md:flex md:items-center md:justify-between">
       <div className="min-w-0 flex-1">
-        <h2 className="text-2xl font-bold leading-7 sm:truncate sm:text-3xl sm:tracking-tight">
-          {title}
-        </h2>
+        {title && (
+          <h2 className="mb-4 text-2xl font-bold leading-7 sm:truncate sm:text-3xl sm:tracking-tight md:mb-0">
+            {title}
+          </h2>
+        )}
       </div>
-      <div className="mt-4 flex md:ml-4 md:mt-0">{buttons}</div>
+      <div className="flex md:ml-4">{buttons}</div>
     </div>
   );
 };
