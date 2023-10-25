bcfdcc6f866705aa739f22c8679e609d92abd7e9
wenbin <wenbinlim93@gmail.com>
2023-10-25 14:02:29 +0800

    moved firebase stuff out of backend.ts, initFirebase in main.tsx

--- src/lib/backend.ts
+++ src/lib/backend.ts
@@ -1,21 +1,3 @@
-/**
- * Firebase auth
- */
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_FIREBASE_APPID,
-  measurementId: import.meta.env.VITE_FIREBASE_MESUREMENTID,
-};
-
-// Initialize Firebase
-export const FIREBASE_APP = initializeApp(firebaseConfig);
-
 import PocketBase, {
   RecordFullListOptions,
   RecordListOptions,
